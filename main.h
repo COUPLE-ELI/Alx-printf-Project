f79fb08630c6dba03a97b7edea8c6b57b03c2cce
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-10-16 04:00:46 -0700

    main.h

--- main.h
+++ main.h
@@ -1,5 +1,14 @@
-#ifndef "main.h"
-#define "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+typedef struct funct_character
+{
+	char specifier;
+	int funct;
+}f-ch;
 
 int _printf(const char *format, ...);
 
