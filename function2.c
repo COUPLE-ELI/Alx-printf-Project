c2b0b0c467abb4868708f399772ac7ad4b8b7253
COUPLE-ELI <couple1999elijah@gmail.com>
2023-10-17 15:23:58 -0700

    task 0

--- function2.c
+++ function2.c
@@ -44,24 +44,24 @@ int print_pointer(va_list types, char buffer[],
 		add_c = ' ', length++;
 	ind++;
 	return (write_pointer(buffer, ind, length,
-	width, flags, padd, add_c, padd_start));
+				width, flags, padd, add_c, padd_start));
 
 }
 
 /************************* PRINT NON PRINTABLE *************************/
 
- /**
-  *
-  * print_non_printable - Prints ascii codes in hexa of non printable chars
-  * @types: Lista of arguments
-  * @buffer: Buffer array to handle print
-  * @flags:  Calculates active flags
-  * @width: get width
-  * @precision: Precision specification
-  * @size: Size specifier
-  * Return: Number of chars printed
-  * 
-  */
+/**
+ *
+ * print_non_printable - Prints ascii codes in hexa of non printable chars
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ * 
+ */
 int print_non_printable(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
